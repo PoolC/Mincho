12b50f765fc2158ea0817f7f76b5b76d7f0340c2
mingd1023 <mingd1023@gamail.com>
2021-01-16 17:42:42 +0900

    Feat: 프로젝트 초기 설정
    풀씨 홈페이지 백엔트 초기 설정
    spring boot 사용

---
+++ src/main/java/poolc/poolc/PoolcApplication.java
@@ -0,0 +1,13 @@
+package poolc.poolc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poolc/poolc/PoolcApplicationTests.java
@@ -0,0 +1,13 @@
+package poolc.poolc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
