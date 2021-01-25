f0a3c19a6def09eda5b20d83a41e3de7358e974f
hyungchul pak <jasotn12@naver.com>
2021-01-25 17:45:02 +0900

    Feat. PostDomain Create @Entity(name = "") 수정

--- src/main/java/poolc/poolc/domain/Comment.java
+++ src/main/java/poolc/poolc/domain/Comment.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 import static javax.persistence.FetchType.LAZY;
 
-@Entity
+@Entity(name = "Comment")
 @Getter
 public class Comment {
     @Id
--- src/main/java/poolc/poolc/domain/Post.java
+++ src/main/java/poolc/poolc/domain/Post.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 
 import static javax.persistence.FetchType.LAZY;
 
-@Entity
+@Entity(name = "Post")
 @Getter
 public class Post {
     @Id
