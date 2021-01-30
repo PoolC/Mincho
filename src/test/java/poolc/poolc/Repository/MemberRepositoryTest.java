6c771e75b1aa839a7f4e0befba511b0d897b5bb5
hyungchul pak <jasotn12@naver.com>
2021-01-30 11:41:30 +0900

    Feat. MemberRepository Create 테스트 코드 추가

--- src/test/java/poolc/poolc/Repository/MemberRepositoryTest.java
+++ src/test/java/poolc/poolc/Repository/MemberRepositoryTest.java
@@ -21,6 +21,8 @@ import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @Transactional
 public class MemberRepositoryTest {
@@ -30,7 +32,6 @@ public class MemberRepositoryTest {
     EntityManager em;
 
     @Test
-    @Rollback(value = false)
     public void 회원가입() throws Exception{
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
@@ -41,9 +42,44 @@ public class MemberRepositoryTest {
         memberRepository.save(member);
     }
 
+
     @Test
-    @Rollback(value = false)
-    public void 회원가입_후_탈퇴() throws Exception{
+    public void 모든회원조회() throws Exception{
+        List<ProjectMember> projectMembers = new ArrayList<>();
+        String a = "ddd";
+        Member member1 = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
+                "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
+                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
+        Member member2 = new Member("2", "ppp", "anfr2520@gmail.com", "010-1111-2222", "ppp",
+                "ComputerScience", "2016147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
+                LocalDateTime.now(),"aaa","나는 ppp이다", false, a.getBytes(), a.getBytes(), projectMembers );
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        List<Member> members = memberRepository.findAll();
+        for (Member member : members) {
+            System.out.println("member = " + member);
+        }
+    }
+
+    @Test
+    public void UUID회원찾기() throws Exception{
+        List<ProjectMember> projectMembers = new ArrayList<>();
+        String a = "ddd";
+        Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "00-4595-9147", "hyungchulpak",
+                "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
+                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
+
+        memberRepository.save(member);
+        em.flush();
+        em.clear();
+        System.out.println(member);
+        Member findMember = memberRepository.findOne("1");
+        member.equals(findMember);
+    }
+
+
+    @Test
+    public void 이메일로회원찾기() throws Exception{
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
         Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
@@ -51,7 +87,25 @@ public class MemberRepositoryTest {
                 LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
 
         memberRepository.save(member);
+        em.flush();
+        em.clear();
+        System.out.println(member);
+        Member findMember = memberRepository.findByEmail("jasotn12@naver.com");
+        member.equals(findMember);
+    }
+
+    @Test
+    public void 회원가입_후_탈퇴() throws Exception{
+        List<ProjectMember> projectMembers = new ArrayList<>();
+        String a = "ddd";
+        Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
+                "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
+                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
+        memberRepository.save(member);
         memberRepository.delete(member);
+
+        Member findMember = memberRepository.findOne("1");
+        assertNotEquals(findMember,member);
     }
 
 }
