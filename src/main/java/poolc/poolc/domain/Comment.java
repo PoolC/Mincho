e6d2fc7a35fc2f31e04fb89d3263d181b31f33e3
hyungchul pak <jasotn12@naver.com>
2021-02-02 17:16:35 +0900

    Feat: Comment domain 수정 CommentRepositoryTest를 위한 코드 수정

--- src/main/java/poolc/poolc/domain/Comment.java
+++ src/main/java/poolc/poolc/domain/Comment.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import javax.persistence.*;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import static javax.persistence.FetchType.LAZY;
 
@@ -33,4 +34,32 @@ public class Comment {
     @Column(name = "updatedAt", nullable = false)
     private LocalDateTime updatedAt;
 
+    public Comment() {
+    }
+
+    public Comment(Post post, Member member, String body, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.post = post;
+        this.member = member;
+        this.body = body;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return Objects.equals(getId(), comment.getId()) &&
+                Objects.equals(getPost(), comment.getPost()) &&
+                Objects.equals(getMember(), comment.getMember()) &&
+                Objects.equals(getBody(), comment.getBody()) &&
+                Objects.equals(getCreatedAt(), comment.getCreatedAt()) &&
+                Objects.equals(getUpdatedAt(), comment.getUpdatedAt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getPost(), getMember(), getBody(), getCreatedAt(), getUpdatedAt());
+    }
 }
