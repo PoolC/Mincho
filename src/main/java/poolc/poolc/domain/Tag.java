2edef97e0d86dacf3dcf1d28201815a3c094e451
hyungchul pak <jasotn12@naver.com>
2021-02-03 17:12:40 +0900

    Feat: Tag Domain 수정

--- src/main/java/poolc/poolc/domain/Tag.java
+++ src/main/java/poolc/poolc/domain/Tag.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity(name = "Tag")
 @Getter
@@ -18,4 +19,24 @@ public class Tag {
     @Column(name = "name", columnDefinition = "varchar(1024)", nullable = false)
     private String name;
 
+    public Tag() {
+    }
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tag tag = (Tag) o;
+        return Objects.equals(getId(), tag.getId()) &&
+                Objects.equals(getName(), tag.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
 }
