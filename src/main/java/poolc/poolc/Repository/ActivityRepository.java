c526f59a57378a44b1e60cf90cc16199039b7a2e
hyungchul pak <jasotn12@naver.com>
2021-02-10 18:21:45 +0900

    Revert "Fix: ActivityRepository 수정"
    This reverts commit e04dedb82992a0635c371f6b2806da933d68367c.

--- src/main/java/poolc/poolc/Repository/ActivityRepository.java
+++ src/main/java/poolc/poolc/Repository/ActivityRepository.java
@@ -1,7 +1,31 @@
 package poolc.poolc.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import poolc.poolc.domain.Activity;
 
-public interface ActivityRepository extends JpaRepository<Activity, Long> {
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ActivityRepository {
+    private final EntityManager em;
+
+    public void save(Activity activity){
+        em.persist(activity);
+    }
+
+    public void delete(Long id){
+        em.remove(em.find(Activity.class,id));
+    }
+
+    public Activity findOne(Long id){
+        return em.find(Activity.class, id);
+    }
+
+    public List<Activity> findAll(){
+        return em.createQuery("select a from Activity a",Activity.class)
+                .getResultList();
+    }
 }
