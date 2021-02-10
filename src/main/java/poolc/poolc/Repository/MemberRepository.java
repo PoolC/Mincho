32c8ce0b513c2438714c6d0a3392fbcaddb41f93
yoonseokch <yoonsukch@gmail.com>
2021-02-10 17:24:22 +0900

    Refactor : Introduce JPA Member repository

--- src/main/java/poolc/poolc/Repository/MemberRepository.java
+++ src/main/java/poolc/poolc/Repository/MemberRepository.java
@@ -1,37 +1,14 @@
 package poolc.poolc.repository;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import poolc.poolc.domain.Member;
 
-import javax.persistence.EntityManager;
-import java.util.List;
+public interface MemberRepository extends JpaRepository<Member, String> {
 
-@Repository
-@RequiredArgsConstructor
-public class MemberRepository{
-    private final EntityManager em;
+    Member findByEmail(String email);
 
-    public void save(Member member){
-        em.persist(member);
-    }
+    Member findByLoginID(String LoginID);
 
-    public Member findOne(String UUID){
-        return em.find(Member.class, UUID);
-    }
+    Member findByStudentID(String StudentID);
 
-    public List<Member> findAll(){
-        return em.createQuery("select m from Member m", Member.class).getResultList();
-    }
-
-    public void delete(Member member){
-        em.remove(member);
-    }
-
-    public Member findByEmail(String email){
-        return em.createQuery("select m from Member m where m.email = :email", Member.class)
-                .setParameter("email", email)
-                .getSingleResult();
-    }
 }
--- src/test/java/poolc/poolc/Repository/MemberRepositoryTest.java
+++ src/test/java/poolc/poolc/Repository/MemberRepositoryTest.java
@@ -12,8 +12,10 @@ import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @Transactional
@@ -24,77 +26,114 @@ public class MemberRepositoryTest {
     EntityManager em;
 
     @Test
-    public void 회원가입() throws Exception{
+    public void 회원가입() {
+        //given
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
         Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
                 "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
-                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
+                LocalDateTime.now(), "aaa", "나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers);
 
+        //when
         memberRepository.save(member);
-        assertEquals(member,memberRepository.findOne("1"));
+
+        //given
+        Optional<Member> findMember = memberRepository.findById(member.getUUID());
+        member.equals(findMember);
     }
 
 
     @Test
-    public void 모든회원조회() throws Exception{
+    public void 모든회원조회() {
+        //given
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
         Member member1 = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
                 "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
-                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
+                LocalDateTime.now(), "aaa", "나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers);
         Member member2 = new Member("2", "ppp", "anfr2520@gmail.com", "010-1111-2222", "ppp",
                 "ComputerScience", "2016147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
-                LocalDateTime.now(),"aaa","나는 ppp이다", false, a.getBytes(), a.getBytes(), projectMembers );
+                LocalDateTime.now(), "aaa", "나는 ppp이다", false, a.getBytes(), a.getBytes(), projectMembers);
+
+        //when
         memberRepository.save(member1);
         memberRepository.save(member2);
+
+        //then
         List<Member> members = memberRepository.findAll();
-        assertEquals(2L,members.size());
+        assertEquals(2L, members.size());
     }
 
     @Test
-    public void UUID회원찾기() throws Exception{
+    public void loginID회원찾기() {
+        //given
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
         Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "00-4595-9147", "hyungchulpak",
                 "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
-                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
+                LocalDateTime.now(), "aaa", "나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers);
 
+        //when
         memberRepository.save(member);
+
+        //then
         em.flush();
         em.clear();
-        Member findMember = memberRepository.findOne("1");
+        Member findMember = memberRepository.findByLoginID(member.getLoginID());
         member.equals(findMember);
     }
 
 
     @Test
-    public void 이메일로회원찾기() throws Exception{
+    public void 이메일로회원찾기() {
+        //given
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
         Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
                 "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
-                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
+                LocalDateTime.now(), "aaa", "나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers);
 
+        //when
         memberRepository.save(member);
+
+        //then
         em.flush();
         em.clear();
-        Member findMember = memberRepository.findByEmail("jasotn12@naver.com");
+        Member findMember = memberRepository.findByEmail(member.getEmail());
         member.equals(findMember);
     }
 
     @Test
-    public void 회원가입_후_탈퇴() throws Exception{
+    public void 학번으로회원찾기() {
+        //given
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
         Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
                 "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
-                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
+                LocalDateTime.now(), "aaa", "나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers);
+
+        //when
+        memberRepository.save(member);
+
+        //then
+        em.flush();
+        em.clear();
+
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        member.equals(findMember);
+    }
+
+    @Test
+    public void 회원가입_후_탈퇴() {
+        List<ProjectMember> projectMembers = new ArrayList<>();
+        String a = "ddd";
+        Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
+                "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
+                LocalDateTime.now(), "aaa", "나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers);
         memberRepository.save(member);
         memberRepository.delete(member);
 
-        Member findMember = memberRepository.findOne("1");
-        assertNull(findMember);
+        Optional<Member> findMember = memberRepository.findById(member.getUUID());
+        assertTrue(findMember.isEmpty());
     }
-
 }
