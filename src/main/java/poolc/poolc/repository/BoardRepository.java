460f42fb4d00fbecc589c67f726888caccf37eb2
hyungchul pak <jasotn12@naver.com>
2021-02-10 18:34:09 +0900

    Refactor: BoardRepository 수정

--- src/main/java/poolc/poolc/repository/BoardRepository.java
+++ src/main/java/poolc/poolc/repository/BoardRepository.java
@@ -1,32 +1,7 @@
 package poolc.poolc.repository;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import poolc.poolc.domain.Board;
 
-import javax.persistence.EntityManager;
-import java.util.List;
-
-@Repository
-@RequiredArgsConstructor
-public class BoardRepository {
-    private final EntityManager em;
-
-    public void save(Board board) {
-        em.persist(board);
-    }
-
-    public void delete(Board board){
-        em.remove(board);
-    }
-
-    public Board findOne(Long id){
-        return em.find(Board.class, id);
-    }
-
-    public List<Board> findAll(){
-        return em.createQuery("select b from Board b", Board.class)
-                .getResultList();
-    }
-
+public interface BoardRepository extends JpaRepository<Board, Long> {
 }
