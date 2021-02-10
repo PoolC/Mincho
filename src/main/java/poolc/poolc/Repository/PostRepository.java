b95b3c286d487a04a64566a326832a43e7f045b4
hyungchul pak <jasotn12@naver.com>
2021-02-10 19:05:20 +0900

    Refactor: PostRepository 수정

--- src/main/java/poolc/poolc/Repository/PostRepository.java
+++ src/main/java/poolc/poolc/Repository/PostRepository.java
@@ -1,42 +1,14 @@
 package poolc.poolc.repository;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import poolc.poolc.domain.Board;
 import poolc.poolc.domain.Member;
 import poolc.poolc.domain.Post;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class PostRepository {
-    private final EntityManager em;
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByBoard(Board board);
 
-    public void save(Post post){
-        em.persist(post);
-    }
-
-    public void delete(Long postID){
-        em.remove(em.find(Post.class,postID));
-    }
-
-    public Post findOne(Long id){
-        return em.find(Post.class, id);
-    }
-
-    public List<Post> findAllByBoard(Board board){
-        return em.createQuery("select p from Post p where p.board = :board", Post.class)
-                .setParameter("board", board)
-                .getResultList();
-    }
-
-    public List<Post> findAllByAuthor(Member member){
-        return em.createQuery("select p from Post p where p.member = :author",Post.class)
-                .setParameter("author", member)
-                .getResultList();
-    }
-
-
+    List<Post> findByMember(Member member);
 }
