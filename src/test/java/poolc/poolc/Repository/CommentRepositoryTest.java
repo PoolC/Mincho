fae20f3f2e124fe110b180c5d9a2f74e3e6c87ee
hyungchul pak <jasotn12@naver.com>
2021-02-10 18:46:32 +0900

    Refactor: CommentRepositoryTest 수정

--- src/test/java/poolc/poolc/Repository/CommentRepositoryTest.java
+++ src/test/java/poolc/poolc/Repository/CommentRepositoryTest.java
@@ -15,6 +15,7 @@ import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
@@ -31,84 +32,95 @@ public class CommentRepositoryTest {
     EntityManager em;
 
     @Test
-    public void 댓글생성() throws Exception{
+    public void 댓글생성() {
         //given
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
         Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
                 "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
-                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
-
+                LocalDateTime.now(), "aaa", "나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers);
         memberRepository.save(member);
+
         Board board = new Board("test", "/test", "read", "write", LocalDateTime.now(), LocalDateTime.now());
         boardRepository.save(board);
+
+        em.flush();
 
         Post post = new Post(board, member, "aaaa", "aaaaaa", LocalDateTime.now(), LocalDateTime.now());
         postRepository.save(post);
 
-        //when
+        em.flush();
+
         Comment comment = new Comment(post, member, "aaaa", LocalDateTime.now(), LocalDateTime.now());
+
+        //when
         commentRepository.save(comment);
 
-
         //then
-        em.flush();
-        em.clear();
-        comment.equals(commentRepository.findOne(comment.getId()));
+        comment.equals(commentRepository.findById(comment.getId()));
     }
 
     @Test
-    public void 댓글삭제() throws Exception{
+    public void 댓글삭제() {
         //given
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
         Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
                 "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
-                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
-
+                LocalDateTime.now(), "aaa", "나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers);
         memberRepository.save(member);
+
         Board board = new Board("test", "/test", "read", "write", LocalDateTime.now(), LocalDateTime.now());
         boardRepository.save(board);
 
+        em.flush();
+
         Post post = new Post(board, member, "aaaa", "aaaaaa", LocalDateTime.now(), LocalDateTime.now());
         postRepository.save(post);
+
+        em.flush();
 
         Comment comment = new Comment(post, member, "aaaa", LocalDateTime.now(), LocalDateTime.now());
         commentRepository.save(comment);
+
         //when
-        commentRepository.delete(comment.getId());
+        commentRepository.delete(comment);
 
         //then
-        em.flush();
-        em.clear();
-        Assertions.assertNull(commentRepository.findOne(comment.getId()));
+        Optional<Comment> findComment = commentRepository.findById(comment.getId());
+        Assertions.assertTrue(findComment.isEmpty());
     }
 
     @Test
-    public void 게시글ID로댓글전체조회() throws Exception{
+    public void 게시글ID로댓글전체조회() {
         //given
         List<ProjectMember> projectMembers = new ArrayList<>();
         String a = "ddd";
         Member member = new Member("1", "anfro2520", "jasotn12@naver.com", "010-4595-9147", "hyungchulpak",
                 "ComputerScience", "2015147514", false, false, LocalDateTime.now(), LocalDateTime.now(), "pass",
-                LocalDateTime.now(),"aaa","나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers );
-
+                LocalDateTime.now(), "aaa", "나는 박형철이다", false, a.getBytes(), a.getBytes(), projectMembers);
         memberRepository.save(member);
+
         Board board = new Board("test", "/test", "read", "write", LocalDateTime.now(), LocalDateTime.now());
         boardRepository.save(board);
 
+        em.flush();
+
         Post post = new Post(board, member, "aaaa", "aaaaaa", LocalDateTime.now(), LocalDateTime.now());
         postRepository.save(post);
+
+        em.flush();
 
         Comment comment1 = new Comment(post, member, "aaaa", LocalDateTime.now(), LocalDateTime.now());
         commentRepository.save(comment1);
 
         Comment comment2 = new Comment(post, member, "bbbb", LocalDateTime.now(), LocalDateTime.now());
         commentRepository.save(comment2);
+
         //when
-        List<Comment> comments = commentRepository.findAllByPost(post.getId());
+        List<Comment> comments = commentRepository.findByPost(post);
+
         //then
         Assertions.assertEquals(2L, comments.size());
     }
-
 }
