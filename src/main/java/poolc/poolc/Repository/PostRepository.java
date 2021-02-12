0f3347019dedfee1e134453404636d9428ea8023
hyungchul pak <jasotn12@naver.com>
2021-02-12 12:08:28 +0900

    Revert "Refactor: PostRepository 수정"
    This reverts commit b95b3c286d487a04a64566a326832a43e7f045b4.

--- src/main/java/poolc/poolc/Repository/PostRepository.java
+++ src/main/java/poolc/poolc/Repository/PostRepository.java
@@ -1,14 +1,42 @@
 package poolc.poolc.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import poolc.poolc.domain.Board;
 import poolc.poolc.domain.Member;
 import poolc.poolc.domain.Post;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
-    List<Post> findByBoard(Board board);
+@Repository
+@RequiredArgsConstructor
+public class PostRepository {
+    private final EntityManager em;
 
-    List<Post> findByMember(Member member);
+    public void save(Post post){
+        em.persist(post);
+    }
+
+    public void delete(Long postID){
+        em.remove(em.find(Post.class,postID));
+    }
+
+    public Post findOne(Long id){
+        return em.find(Post.class, id);
+    }
+
+    public List<Post> findAllByBoard(Board board){
+        return em.createQuery("select p from Post p where p.board = :board", Post.class)
+                .setParameter("board", board)
+                .getResultList();
+    }
+
+    public List<Post> findAllByAuthor(Member member){
+        return em.createQuery("select p from Post p where p.member = :author",Post.class)
+                .setParameter("author", member)
+                .getResultList();
+    }
+
+
 }
