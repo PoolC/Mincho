95c3075dbb3df3e1e082a5ad43b3015f9e8fc79c
hyungchul pak <jasotn12@naver.com>
2021-02-12 12:08:26 +0900

    Revert "Refactor: CommentRepository 수정"
    This reverts commit 6310d8b4195e115e5a977581cb0f7a395e855184.

--- src/main/java/poolc/poolc/Repository/CommentRepository.java
+++ src/main/java/poolc/poolc/Repository/CommentRepository.java
@@ -1,11 +1,33 @@
 package poolc.poolc.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import poolc.poolc.domain.Comment;
 import poolc.poolc.domain.Post;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPost(Post post);
+@Repository
+@RequiredArgsConstructor
+public class CommentRepository {
+    private final EntityManager em;
+
+    public void save(Comment comment){
+        em.persist(comment);
+    }
+
+    public void delete(Long commentID){
+        em.remove(em.find(Comment.class,commentID));
+    }
+
+    public Comment findOne(Long commentID){
+        return em.find(Comment.class,commentID);
+    }
+
+    public List<Comment> findAllByPost(Long PostID){
+        return em.createQuery("select c from Comment c where c.post = :post",Comment.class)
+                .setParameter("post",em.find(Post.class, PostID))
+                .getResultList();
+    }
 }
