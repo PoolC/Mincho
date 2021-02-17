70c130629d1ffb6f805a2877629195b064dcd459
hyungchul pak <jasotn12@naver.com>
2021-02-17 14:27:50 +0900

    Refactor: 메소드명 변경 및 메소드 로직 추가

--- src/main/java/org/poolc/api/member/infra/BearerAuthInterceptor.java
+++ src/main/java/org/poolc/api/member/infra/BearerAuthInterceptor.java
@@ -2,6 +2,7 @@ package org.poolc.api.member.infra;
 
 import org.poolc.api.auth.infra.JwtTokenProvider;
 import org.poolc.api.auth.vo.ParsedTokenValues;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class BearerAuthInterceptor implements HandlerInterceptor {
     private JwtTokenProvider jwtTokenProvider;
 
+    @Autowired
     public BearerAuthInterceptor(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
@@ -22,14 +24,20 @@ public class BearerAuthInterceptor implements HandlerInterceptor {
         if (request.getMethod().equals("POST")) {
             return true;
         }
-        String token = removeBearerFromToken(request.getHeader("authorization"));
+        String token = parsingJwtToken(request.getHeader("authorization"));
         ParsedTokenValues userInfo = jwtTokenProvider.getUserInfo(token);
         request.setAttribute("UUID", userInfo.getUUID());
         request.setAttribute("isAdmin", userInfo.getIsAdmin());
         return true;
     }
 
-    String removeBearerFromToken(String token) {
-        return token.substring(7);
+    private String parsingJwtToken(String token) {
+        if (token.contains("Bearer"))
+            return token.replace("Bearer ", "");
+
+        if (token.contains("Basic"))
+            return token.replace("Basic ", "");
+
+        return token;
     }
 }
