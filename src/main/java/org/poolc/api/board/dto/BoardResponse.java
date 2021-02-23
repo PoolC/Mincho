d9e3fccbeff53a494afb41fe639740b36734646f
hyungchul pak <jasotn12@naver.com>
2021-02-23 14:41:13 +0900

    Refactor: BoardResponse에 id 컬럼 추가

--- src/main/java/org/poolc/api/board/dto/BoardResponse.java
+++ src/main/java/org/poolc/api/board/dto/BoardResponse.java
@@ -6,6 +6,7 @@ import org.poolc.api.board.domain.Board;
 
 @Getter
 public class BoardResponse {
+    private final Long ID;
     private final String name;
     private final String urlPath;
     private final String readPermission;
@@ -13,6 +14,7 @@ public class BoardResponse {
 
     @JsonCreator
     public BoardResponse(Board board) {
+        this.ID = board.getId();
         this.name = board.getName();
         this.urlPath = board.getUrlPath();
         this.readPermission = board.getReadPermission();
