d0ee756d63ef0ca892c27ea3f218ecee07c3e747
yoonseokch <yoonsukch@gmail.com>
2021-02-24 15:32:22 +0900

    Fix: 유효하지않은 접근 controller 수정

--- src/main/java/org/poolc/api/common/controller/ErrorController.java
+++ src/main/java/org/poolc/api/common/controller/ErrorController.java
@@ -5,12 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RestController
 public class ErrorController {
     @GetMapping("/error")
-    public ResponseEntity<String> unauthenticatedHandler(HttpServletRequest request) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(request.getAttribute("message").toString());
+    public ResponseEntity<String> unauthenticatedHandler() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 접근입니다");
     }
 }
