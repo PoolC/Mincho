7c5371b705479a591ffa16dbd03d598985bcd4e9
yoonseokch <yoonsukch@gmail.com>
2021-03-02 17:31:21 +0900

    Fix: resolve merge conflict

--- src/main/java/org/poolc/api/member/controller/MemberController.java
+++ src/main/java/org/poolc/api/member/controller/MemberController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -32,6 +33,15 @@ public class MemberController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MemberListResponse> getAllMembers(@AuthenticationPrincipal Member member) {
         List<MemberResponse> memberList = memberService.getAllMembers()
+                .stream().map(MemberResponse::of)
+                .collect(Collectors.toList());
+        MemberListResponse MemberListResponses = new MemberListResponse(memberList);
+        return ResponseEntity.ok().body(MemberListResponses);
+    }
+
+    @GetMapping(value = "/name", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MemberListResponse> findMembersForProject(HttpServletRequest request) {
+        List<MemberResponse> memberList = memberService.getAllMembersByName(request.getParameter("name"))
                 .stream().map(MemberResponse::of)
                 .collect(Collectors.toList());
         MemberListResponse MemberListResponses = new MemberListResponse(memberList);
