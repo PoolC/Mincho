749e0924717130f7ca87dd35074b513ae05f7052
hyungchul pak <jasotn12@naver.com>
2021-03-03 20:45:41 +0900

    Refactor: post에 있는 UnauthorizedException.java 삭제

--- src/main/java/org/poolc/api/post/exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package org.poolc.api.post.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
