039d91b10323292cbf4d79234abe09c1b527c9d4
hyungchul pak <jasotn12@naver.com>
2021-03-03 20:46:14 +0900

    Fix: dto 관련 error 제거

--- src/main/java/org/poolc/api/comment/dto/UpdateCommentRequest.java
+++ src/main/java/org/poolc/api/comment/dto/UpdateCommentRequest.java
@@ -1,11 +1,13 @@
 package org.poolc.api.comment.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 
 @Getter
 public class UpdateCommentRequest {
     private String body;
 
+    @JsonCreator
     public UpdateCommentRequest(String body) {
         this.body = body;
     }
