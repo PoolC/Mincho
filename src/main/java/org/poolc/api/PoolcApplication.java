df1dfca9449ab6559f7418fc4f85a987ecc60746
Jjungs7 <jeongy95@gmail.com>
2021-03-14 14:36:33 +0900

    refactor: cors with option headers

--- src/main/java/org/poolc/api/PoolcApplication.java
+++ src/main/java/org/poolc/api/PoolcApplication.java
@@ -2,9 +2,6 @@ package org.poolc.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PoolcApplication {
@@ -13,14 +10,4 @@ public class PoolcApplication {
         SpringApplication.run(PoolcApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*")
-                        .allowedHeaders("GET", "POST", "PUT", "DELETE", "OPTIONS");
-            }
-        };
-    }
 }
