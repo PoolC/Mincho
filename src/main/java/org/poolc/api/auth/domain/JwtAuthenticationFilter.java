1820877c2ccc307b546b5c80782aa8129e22d1ea
hyungchul pak <jasotn12@naver.com>
2021-03-15 23:00:52 +0900

    Fix: unauthenticateException 발생시 500 에러 뜨는 걸 401으로 보내도록 임시로 작성

--- src/main/java/org/poolc/api/auth/domain/JwtAuthenticationFilter.java
+++ src/main/java/org/poolc/api/auth/domain/JwtAuthenticationFilter.java
@@ -1,11 +1,11 @@
 package org.poolc.api.auth.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.poolc.api.auth.exception.UnauthenticatedException;
 import org.poolc.api.auth.infra.JwtTokenProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -13,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Optional;
 
@@ -25,15 +26,21 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         String token = jwtTokenProvider.getToken((HttpServletRequest) request);
+        //TODO: 임시로 try catch를 통해 해결
+        try {
+            Optional.ofNullable(token)
+                    .map(jwtTokenProvider::getSubject)
+                    .map(userDetailsService::loadUserByUsername)
+                    .map(userDetails -> new UsernamePasswordAuthenticationToken(userDetails,
+                            "",
+                            userDetails.getAuthorities()))
+                    .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
 
-        Optional.ofNullable(token)
-                .map(jwtTokenProvider::getSubject)
-                .map(userDetailsService::loadUserByUsername)
-                .map(userDetails -> new UsernamePasswordAuthenticationToken(userDetails,
-                        "",
-                        userDetails.getAuthorities()))
-                .ifPresent(authentication -> SecurityContextHolder.getContext().setAuthentication(authentication));
+            chain.doFilter(request, response);
 
-        chain.doFilter(request, response);
+        } catch (UnauthenticatedException e) {
+            HttpServletResponse res = (HttpServletResponse) response;
+            res.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+        }
     }
 }
