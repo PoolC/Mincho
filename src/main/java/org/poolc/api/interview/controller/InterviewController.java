0ef8a1af3cf2d5136a64a29b599da2ecf0f9110e
hcpak <jasotn12@naver.com>
2021-07-19 15:54:10 +0900

    feature: api 명세 바뀜에 따라 controller 변경

--- src/main/java/org/poolc/api/interview/controller/InterviewController.java
+++ src/main/java/org/poolc/api/interview/controller/InterviewController.java
@@ -1,20 +1,16 @@
 package org.poolc.api.interview.controller;
 
 import org.poolc.api.interview.dto.InterviewSlotResponse;
+import org.poolc.api.interview.dto.InterviewTableResponse;
 import org.poolc.api.interview.dto.RegisterInterviewSlotRequest;
 import org.poolc.api.interview.dto.UpdateInterviewSlotRequest;
 import org.poolc.api.member.domain.Member;
 import org.springframework.http.ResponseEntity;
 
 import javax.naming.NoPermissionException;
-import java.util.List;
 
 public interface InterviewController {
-    public ResponseEntity<List<InterviewSlotResponse>> getInterviewTable(Member member);
-
-    public ResponseEntity<InterviewSlotResponse> applyInterviewSlot(Member member, Long slotId) throws NoPermissionException;
-
-    public ResponseEntity<Void> cancelInterviewSlot(Member member, Long slotId);
+    public ResponseEntity<InterviewTableResponse> getInterviewTable(Member member);
 
     public ResponseEntity<Void> enrollInterviewSlot(Member member, RegisterInterviewSlotRequest request);
 
@@ -23,4 +19,8 @@ public interface InterviewController {
     public ResponseEntity<Void> deleteInterviewSlot(Member member, Long slotId);
 
     public ResponseEntity<Void> deleteAllInterviewSlot(Member member);
+    
+    public ResponseEntity<InterviewSlotResponse> applyInterviewSlot(Member member, Long slotId) throws NoPermissionException;
+
+    public ResponseEntity<Void> cancelInterviewSlot(Member member, Long slotId);
 }
--- src/main/java/org/poolc/api/interview/controller/InterviewControllerImpl.java
+++ src/main/java/org/poolc/api/interview/controller/InterviewControllerImpl.java
@@ -2,9 +2,7 @@ package org.poolc.api.interview.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.poolc.api.interview.domain.InterviewSlot;
-import org.poolc.api.interview.dto.InterviewSlotResponse;
-import org.poolc.api.interview.dto.RegisterInterviewSlotRequest;
-import org.poolc.api.interview.dto.UpdateInterviewSlotRequest;
+import org.poolc.api.interview.dto.*;
 import org.poolc.api.interview.service.InterviewService;
 import org.poolc.api.member.domain.Member;
 import org.springframework.http.MediaType;
@@ -13,7 +11,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.naming.NoPermissionException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -22,24 +22,18 @@ import java.util.stream.Collectors;
 public class InterviewControllerImpl implements InterviewController {
     private final InterviewService interviewService;
 
-    @GetMapping("/table")
-    public ResponseEntity<List<InterviewSlotResponse>> getInterviewTable(@AuthenticationPrincipal Member member) {
-        List<InterviewSlotResponse> response = interviewService.getAllInterviewSlot()
-                .stream().map(slot -> new InterviewSlotResponse(slot, member.isAdmin()))
-                .collect(Collectors.toList());
+    @GetMapping("/slots")
+    public ResponseEntity<InterviewTableResponse> getInterviewTable(@AuthenticationPrincipal Member member) {
+        List<InterviewSlotsByDateResponse> data = new ArrayList<>();
+        interviewService.getAllInterviewSlot().stream().map(slot -> new InterviewSlotResponse(slot, member.isAdmin()))
+                .collect(Collectors.groupingBy(InterviewSlotResponse::getDate))
+                .forEach((key, value) -> data.add(new InterviewSlotsByDateResponse(key, value)));
+
+        InterviewTableResponse response = new InterviewTableResponse(Optional.ofNullable(member)
+                .map(Member::getInterviewSlot)
+                .map(InterviewSlot::getId)
+                .orElse(null), data);
         return ResponseEntity.ok().body(response);
-    }
-
-    @PostMapping(value = "/application/{slotId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<InterviewSlotResponse> applyInterviewSlot(@AuthenticationPrincipal Member member, @PathVariable Long slotId) throws NoPermissionException {
-        InterviewSlot slot = interviewService.applyInterviewSlot(member, slotId);
-        return ResponseEntity.accepted().body(new InterviewSlotResponse(slot, member.isAdmin()));
-    }
-
-    @DeleteMapping(value = "/application/{slotId}")
-    public ResponseEntity<Void> cancelInterviewSlot(@AuthenticationPrincipal Member member, @PathVariable Long slotId) {
-        interviewService.cancelApplicationInterviewSlot(member, slotId);
-        return ResponseEntity.accepted().build();
     }
 
     @PostMapping(value = "/slots")
@@ -65,4 +59,18 @@ public class InterviewControllerImpl implements InterviewController {
         interviewService.deleteAllInterviewSlots(member);
         return ResponseEntity.accepted().build();
     }
+
+    @PostMapping(value = "/application/{slotId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<InterviewSlotResponse> applyInterviewSlot(@AuthenticationPrincipal Member member, @PathVariable Long slotId) throws NoPermissionException {
+        InterviewSlot slot = interviewService.applyInterviewSlot(member, slotId);
+        return ResponseEntity.accepted().body(new InterviewSlotResponse(slot, member.isAdmin()));
+    }
+
+    @DeleteMapping(value = "/application/{slotId}")
+    public ResponseEntity<Void> cancelInterviewSlot(@AuthenticationPrincipal Member member, @PathVariable Long slotId) {
+        interviewService.cancelApplicationInterviewSlot(member, slotId);
+        return ResponseEntity.accepted().build();
+    }
+
+
 }
