298f5ddf1692932bf779eba13bf9f34f67f1ebe5
hcpak <jasotn12@naver.com>
2021-08-18 15:08:13 +0900

    feat: 면접 시간 신청 및 면접 시간 신청 취소 response 구조 변경으로 인한 service와 controller 변경 - service와 controller refactoring 진행 - InterviewService와 InterviewController interface 제거

--- src/main/java/org/poolc/api/interview/controller/InterviewController.java
+++ src/main/java/org/poolc/api/interview/controller/InterviewController.java
@@ -1,26 +1,68 @@
 package org.poolc.api.interview.controller;
 
-import org.poolc.api.interview.dto.InterviewSlotResponse;
+import lombok.RequiredArgsConstructor;
+import org.poolc.api.interview.domain.InterviewSlot;
 import org.poolc.api.interview.dto.InterviewTableResponse;
 import org.poolc.api.interview.dto.RegisterInterviewSlotRequest;
 import org.poolc.api.interview.dto.UpdateInterviewSlotRequest;
+import org.poolc.api.interview.service.InterviewService;
 import org.poolc.api.member.domain.Member;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import javax.naming.NoPermissionException;
 
-public interface InterviewController {
-    public ResponseEntity<InterviewTableResponse> getInterviewTable(Member member);
+@RestController
+@RequestMapping("/interview")
+@RequiredArgsConstructor
+public class InterviewController {
+    private final InterviewService interviewService;
 
-    public ResponseEntity<Void> enrollInterviewSlot(Member member, RegisterInterviewSlotRequest request);
+    @GetMapping("/slots")
+    public ResponseEntity<InterviewTableResponse> getInterviewTable(@AuthenticationPrincipal Member member) {
+        InterviewTableResponse response = interviewService.getInterviewTable(member);
+        return ResponseEntity.ok().body(response);
+    }
 
-    public ResponseEntity<Void> updateInterviewSlot(Member member, Long slotId, UpdateInterviewSlotRequest request) throws NoPermissionException;
+    @PostMapping(value = "/slots")
+    public ResponseEntity<Void> enrollInterviewSlot(@AuthenticationPrincipal Member member, @RequestBody RegisterInterviewSlotRequest request) {
+        interviewService.createInterviewSlot(member, request);
+        return ResponseEntity.accepted().build();
+    }
 
-    public ResponseEntity<Void> deleteInterviewSlot(Member member, Long slotId);
+    @PutMapping(value = "/slots/{slotId}")
+    public ResponseEntity<Void> updateInterviewSlot(@AuthenticationPrincipal Member member, @PathVariable Long slotId, @RequestBody UpdateInterviewSlotRequest request) throws NoPermissionException {
+        interviewService.updateInterviewSlot(member, slotId, request);
+        return ResponseEntity.accepted().build();
+    }
 
-    public ResponseEntity<Void> deleteAllInterviewSlot(Member member);
-    
-    public ResponseEntity<InterviewSlotResponse> applyInterviewSlot(Member member, Long slotId) throws NoPermissionException;
+    @DeleteMapping(value = "/slots/{slotId}")
+    public ResponseEntity<Void> deleteInterviewSlot(@AuthenticationPrincipal Member member, @PathVariable Long slotId) {
+        interviewService.deleteInterviewSlot(member, slotId);
+        return ResponseEntity.accepted().build();
+    }
 
-    public ResponseEntity<Void> cancelInterviewSlot(Member member, Long slotId);
+    @DeleteMapping(value = "/slots")
+    public ResponseEntity<Void> deleteAllInterviewSlot(@AuthenticationPrincipal Member member) {
+        interviewService.deleteAllInterviewSlots(member);
+        return ResponseEntity.accepted().build();
+    }
+
+    @PostMapping(value = "/application/{slotId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<InterviewTableResponse> applyInterviewSlot(@AuthenticationPrincipal Member member, @PathVariable Long slotId) throws NoPermissionException {
+        InterviewSlot slot = interviewService.applyInterviewSlot(member, slotId);
+        InterviewTableResponse response = interviewService.getInterviewTable(member);
+        return ResponseEntity.accepted().body(response);
+    }
+
+    @DeleteMapping(value = "/application/{slotId}")
+    public ResponseEntity<InterviewTableResponse> cancelInterviewSlot(@AuthenticationPrincipal Member member, @PathVariable Long slotId) {
+        interviewService.cancelApplicationInterviewSlot(member, slotId);
+        InterviewTableResponse response = interviewService.getInterviewTable(member);
+        return ResponseEntity.accepted().body(response);
+    }
+
+
 }
--- src/main/java/org/poolc/api/interview/service/InterviewService.java
+++ src/main/java/org/poolc/api/interview/service/InterviewService.java
@@ -1,25 +1,120 @@
 package org.poolc.api.interview.service;
 
+import lombok.RequiredArgsConstructor;
+import org.poolc.api.auth.exception.UnauthorizedException;
 import org.poolc.api.interview.domain.InterviewSlot;
-import org.poolc.api.interview.dto.RegisterInterviewSlotRequest;
-import org.poolc.api.interview.dto.UpdateInterviewSlotRequest;
+import org.poolc.api.interview.dto.*;
+import org.poolc.api.interview.repository.InterviewSlotRepository;
 import org.poolc.api.member.domain.Member;
+import org.poolc.api.member.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.naming.NoPermissionException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
-public interface InterviewService {
-    public List<InterviewSlot> getAllInterviewSlot();
+@Service
+@RequiredArgsConstructor
+public class InterviewService {
+    private final InterviewSlotRepository interviewSlotRepository;
+    private final MemberRepository memberRepository;
 
-    public InterviewSlot applyInterviewSlot(Member member, Long slotId) throws NoPermissionException;
+    @Transactional(readOnly = true)
+    public InterviewTableResponse getInterviewTable(Member loginMember) {
+        List<InterviewSlotsByDateResponse> interviewSlotsResponses = new ArrayList<>();
+        interviewSlotRepository.findAllByFetchJoin().stream()
+                .map(slot -> new InterviewSlotResponse(slot, loginMember.isAdmin()))
+                .collect(Collectors.groupingBy(InterviewSlotResponse::getDate))
+                .forEach((k, v) -> interviewSlotsResponses.add(new InterviewSlotsByDateResponse(k, v)));
 
-    public void cancelApplicationInterviewSlot(Member member, Long slotId);
+        InterviewTableResponse interviewTableResponse = new InterviewTableResponse(Optional.ofNullable(loginMember)
+                .map(Member::getInterviewSlot)
+                .map(InterviewSlot::getId)
+                .orElse(null), interviewSlotsResponses);
+        return interviewTableResponse;
+    }
 
-    public void createInterviewSlot(Member admin, RegisterInterviewSlotRequest request);
+    @Transactional
+    public InterviewSlot applyInterviewSlot(Member member, Long slotId) throws NoPermissionException {
+        InterviewSlot slot = getInterviewSlot(slotId);
+        checkIntervieweesAcceptable(slot);
+        member.applyInterviewSlot(slot);
+        slot.insertMember(member);
+        memberRepository.saveAndFlush(member);
+        return slot;
+    }
 
-    public void updateInterviewSlot(Member admin, Long updateSlotId, UpdateInterviewSlotRequest request) throws NoPermissionException;
+    @Transactional
+    public void cancelApplicationInterviewSlot(Member member, Long slotId) {
+        InterviewSlot slot = getInterviewSlot(slotId);
+        member.cancelInterviewSlot(slotId);
+        slot.deleteMember(member);
+        memberRepository.saveAndFlush(member);
+    }
 
-    public void deleteInterviewSlot(Member admin, Long deleteSlotId);
+    @Transactional
+    public void createInterviewSlot(Member admin, RegisterInterviewSlotRequest request) {
+        checkAdmin(admin);
+        checkDuplicateSlotWithDateAndStartTimeAndEndTime(request);
+        interviewSlotRepository.save(new InterviewSlot(request));
+    }
 
-    public void deleteAllInterviewSlots(Member admin);
+    @Transactional
+    public void updateInterviewSlot(Member admin, Long updateSlotId, UpdateInterviewSlotRequest request) throws NoPermissionException {
+        checkAdmin(admin);
+        InterviewSlot slot = getInterviewSlot(updateSlotId);
+        checkUpdateCapacity(request, slot);
+        checkDuplicateSlotWithDateAndStartTimeAndEndTime(request, slot);
+
+        slot.update(request);
+        interviewSlotRepository.saveAndFlush(slot);
+    }
+
+    @Transactional
+    public void deleteInterviewSlot(Member admin, Long deleteSlotId) {
+        checkAdmin(admin);
+        getInterviewSlot(deleteSlotId);
+        interviewSlotRepository.delete(getInterviewSlot(deleteSlotId));
+    }
+
+    @Transactional
+    public void deleteAllInterviewSlots(Member admin) {
+        checkAdmin(admin);
+        interviewSlotRepository.deleteAll();
+    }
+
+    private InterviewSlot getInterviewSlot(Long slotId) {
+        return interviewSlotRepository.findById(slotId)
+                .orElseThrow(() -> new NoSuchElementException("No slot found with given slotId"));
+    }
+
+    private void checkAdmin(Member member) {
+        if (!member.isAdmin())
+            throw new UnauthorizedException("No permission to create Interview slot");
+    }
+
+    private void checkIntervieweesAcceptable(InterviewSlot slot) throws NoPermissionException {
+        if (!slot.checkIntervieweesAcceptable()) {
+            throw new NoPermissionException("It's over capacity");
+        }
+    }
+
+    private void checkDuplicateSlotWithDateAndStartTimeAndEndTime(RegisterInterviewSlotRequest request) {
+        if (interviewSlotRepository.findByDateAndStartTimeAndEndTime(request.getDate(), request.getStartTime(), request.getEndTime()).isPresent())
+            throw new IllegalArgumentException("exist duplicate slot with date, startTime, endTime");
+    }
+
+    private void checkDuplicateSlotWithDateAndStartTimeAndEndTime(UpdateInterviewSlotRequest request, InterviewSlot interviewSlot) {
+        if (interviewSlotRepository.findByDateAndStartTimeAndEndTime(interviewSlot.getDate(), request.getStartTime(), request.getEndTime()).isPresent())
+            throw new IllegalArgumentException("exist duplicate slot with date, startTime, endTime");
+    }
+
+    private void checkUpdateCapacity(UpdateInterviewSlotRequest request, InterviewSlot slot) throws NoPermissionException {
+        if (!slot.checkUpdateCapacity(request.getCapacity()))
+            throw new NoPermissionException("The capacity to modify is less than current capacity");
+    }
 }
