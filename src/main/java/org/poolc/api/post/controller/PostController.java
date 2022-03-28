90584ea1c846063b6e7bbea3715439fa55e5cc58
hcpak <jasotn12@naver.com>
2022-03-28 20:54:03 +0900

    refactor: elastic search 코드 부분 삭제

--- src/main/java/org/poolc/api/post/controller/PostController.java
+++ src/main/java/org/poolc/api/post/controller/PostController.java
@@ -1,14 +1,10 @@
 package org.poolc.api.post.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.poolc.api.board.domain.Board;
 import org.poolc.api.board.service.BoardService;
 import org.poolc.api.member.domain.Member;
 import org.poolc.api.post.dto.*;
 import org.poolc.api.post.service.PostService;
-import org.poolc.api.post.vo.PostSearchValues;
-import org.poolc.api.search.service.PostSearcherService;
-import org.poolc.api.search.vo.PostSearchResult;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,7 +19,6 @@ import java.util.stream.Collectors;
 public class PostController {
     private final BoardService boardService;
     private final PostService postService;
-    private final PostSearcherService postSearcherService;
 
     @PostMapping
     public ResponseEntity<Map<String, Long>> createPost(@AuthenticationPrincipal Member writer,
@@ -56,33 +51,6 @@ public class PostController {
         return ResponseEntity.ok().body(responseBody);
     }
 
-    @GetMapping(value="/search")
-    public ResponseEntity<PostsResponse> searchPostBySingleTerm(@AuthenticationPrincipal Member user,
-                                                                @RequestParam String query) {
-        List<String> boardNames = user.getAccessibleBoards(boardService.getAllBoards())
-                .stream()
-                .map(Board::getUrlPath)
-                .collect(Collectors.toList());
-
-        PostSearchValues values = new PostSearchValues(query, query, query, boardNames);
-        PostsResponse posts = PostsResponse.of(searchPosts(user, values));
-        return ResponseEntity.ok(posts);
-    }
-
-    @PostMapping(value="/search")
-    public ResponseEntity<PostsResponse> advancedPostSearch(@AuthenticationPrincipal Member user,
-                                                                 @RequestBody PostSearchRequest request) {
-        List<String> boardNames = user.getAccessibleBoards(boardService.getAllBoards())
-                .stream()
-                .map(Board::getUrlPath)
-                .collect(Collectors.toList());
-
-        PostSearchValues values = new PostSearchValues(request.getTitle(), request.getBody(), request.getAuthor(),
-                boardNames);
-        PostsResponse posts = PostsResponse.of(searchPosts(user, values));
-        return ResponseEntity.ok(posts);
-    }
-
     @PutMapping(value = "/{postId}")
     public ResponseEntity<Void> updatePost(@AuthenticationPrincipal Member user,
                                            @PathVariable Long postId,
@@ -95,14 +63,5 @@ public class PostController {
     public ResponseEntity<Void> deletePost(@AuthenticationPrincipal Member user, @PathVariable Long postId) {
         postService.deletePost(user, postId);
         return ResponseEntity.ok().build();
-    }
-
-    private List<PostResponse> searchPosts(Member user, PostSearchValues values) {
-        PostSearchResult result = postSearcherService.search(values);
-
-        return result.getPostIds().stream()
-                .map(postId -> postService.getPost(user, postId))
-                .map(PostResponse::of)
-                .collect(Collectors.toList());
     }
 }
