488cdb27d2b4220b2d1fd19bd70a17bd436b5217
hcpak <jasotn12@naver.com>
2022-10-24 22:29:45 +0900

    fix: gmail에서 naver mail 전송 방식으로 교체

--- src/main/java/org/poolc/api/member/service/MailService.java
+++ src/main/java/org/poolc/api/member/service/MailService.java
@@ -16,10 +16,11 @@ public class MailService {
     public void sendEmailPasswordResetToken(String email, String resetPasswordToken) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, "utf-8");
+        helper.setFrom("poolc_official@naver.com");
         helper.setSubject("풀씨 홈페이지 비밀번호 재설정 안내메일입니다.");
         helper.setText("안녕하세요.\n Poolc 홈페이지 비밀번호 재설정 안내 메일입니다. \n\n" +
                 "아래 링크를 눌러 비밀번호 재설정을 진행해주세요.\n" +
-                "<https://poolc.org/password/reset?token=" + resetPasswordToken + ">\n" +
+                "< https://poolc.org/password/reset?token=" + resetPasswordToken + " >\n" +
                 "이 링크는 24시간 동안 유효합니다.\n" +
                 "감사합니다.", false);
         helper.setTo(email);
